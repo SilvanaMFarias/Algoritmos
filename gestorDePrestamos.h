fba53cd610891e91786c13996c58674777961141
AgusFiorda <agus.fiorda.99@gmail.com>
2025-06-30 20:38:54 -0300

    se agrega analisis de prestamos y se suma al menu

--- gestorDePrestamos.h
+++ gestorDePrestamos.h
@@ -4,8 +4,12 @@
 #include "biblioteca.h"
 #include "prestamo.h"
 #include "ABB.h"
-#include <bits/stdc++.h>
+#include <map>
+#include <vector>
 #include <string>
+#include <iostream>
+#include <iomanip>
+#include <limits>
 using namespace std;
 
 class GestorDePrestamos {
@@ -24,6 +28,14 @@ public:
   void cantidadPrestamo(int fecha1, int fecha2);
   void cargaDePrestamo(int carga, int partida);
   void buscarPrestamo(string isbn);
+  
+  // Funciones de análisis de préstamos
+  void mostrarComplejidadAlgoritmica(string operacion, string complejidad, string explicacion);
+  int convertirFechaStringAInt(string fecha);
+  string convertirFechaIntAString(int fecha);
+  void totalPrestamosPorBibliotecaEnRangoDeFechas();
+  void detectarBibliotecasConAltaCarga();
+  void buscarPrestamosDeUsuarioPorISBN();
 };
 
 #endif 
